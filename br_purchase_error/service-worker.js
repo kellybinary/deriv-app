1aaf0b40841f22947c59e6d7c8d65b56ad6b43ac
Kelly Anyi <kelly@binary.com>
2019-12-06 14:54:33 +0800

    Updates

--- br_purchase_error/service-worker.js
+++ br_purchase_error/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/br_purchase_error/precache-manifest.8626e90defde0a59c9045ab9e754b263.js"
+  "/br_purchase_error/precache-manifest.0a319aace2f392baf993cde559d307a7.js"
 );
 
 workbox.core.skipWaiting();
