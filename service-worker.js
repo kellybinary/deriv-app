1da83a2c55c325fe064267d83e3ba80a52e80614
Kelly Anyi <kelly@binary.com>
2020-01-03 15:10:44 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0d8b9e93ef786d7e74281e2b742b5669.js"
+  "/precache-manifest.81f3799d46f9f5c7683b26d82a7b38de.js"
 );
 
 workbox.core.skipWaiting();
