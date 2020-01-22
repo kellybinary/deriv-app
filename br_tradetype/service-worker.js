695cd7c037f7ee26d4bf524967ee3c89a1e6e071
Kelly Anyi <kelly@binary.com>
2020-01-22 18:30:59 +0800

    Updates

--- br_tradetype/service-worker.js
+++ br_tradetype/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/br_tradetype/precache-manifest.3186b9c7c3cb3626225624e30f947cf5.js"
+  "/br_tradetype/precache-manifest.c8545021f7e2ffb550307a92014bb6ef.js"
 );
 
 workbox.core.skipWaiting();
